95fad43fe551fa6b9cff49fac7b24da89d7f996d
Gulshanjangid <gulshanjangid333@gmail.com>
2026-09-15 12:15:25 +0530

    GenAI

---
+++ fastapi-genai-project/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from dotenv import load_dotenv
+from google import genai
+import os
+
+
+# Load .env
+load_dotenv()
+
+app = FastAPI()
+
+
+# Get Gemini API key
+api_key = os.getenv("GEMINI_API_KEY")
+
+print("Gemini API key loaded:", bool(api_key))
+
+
+# Create Gemini client
+client = genai.Client(api_key=api_key)
+
+
+class Question(BaseModel):
+    question: str
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "FastAPI + Gemini AI is running"
+    }
+
+
+@app.post("/ask")
+def ask_ai(data: Question):
+
+    response = client.models.generate_content(
+        model="gemini-3.8-flash",
+        contents=data.question
+    )
+
+    return {
+        "question": data.question,
+        "answer": response.text
+    }
